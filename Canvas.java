60ccbaa4212f28e25ee0217c0e591967227062db
Sam-Kap1an <sfkaplan@calpoly.edu>
2024-12-11 14:20:47 -0800

    canvas

--- Canvas.java
+++ Canvas.java
@@ -1,15 +1,16 @@
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.beans.PropertyChangeListener;
 import java.util.ArrayList;
 import java.util.List;
+
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
-public class Canvas extends JPanel implements KeyListener, ActionListener {
+public class Canvas extends JPanel implements KeyListener, PropertyChangeListener {
 
     Player player;
     List<Platform> platforms;
